c6a6c82a2f501e5a60e6ec49a3bdf4b3fa13aac8
SachaFrdr <96545705+SachaFrdr@users.noreply.github.com>
2023-03-26 23:34:07 +0200

    Update scrap.sh

--- scrap.sh
+++ scrap.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/bash
 
-curl https://www.coingecko.com/en/coins/bitcoin/historical_data#panel -o output.csv 
+curl https://www.coingecko.com/en/coin/magic/historical_data#panel -o output.csv
 html2text output.csv > parsed_data.csv 
 sed -n '/(Historical Data)/,$p' parsed_data.csv > price_data.csv 
 sed -n '/Date/,$p' price_data.csv > price_data2.csv  
@@ -9,12 +9,12 @@ sed -i '$ d' price_data2.csv
 sed -i '1d' price_data2.csv
 sed -i '1iDate Market_Cap Volume Open Close' price_data2.csv
 
-mv price_data2.csv data_btc.csv
+mv price_data2.csv data_magic.csv
 rm output.csv 
 rm parsed_data.csv 
 rm price_data.csv 
 
-curl https://www.coingecko.com/en/coins/tether/historical_data#panel -o output.csv 
+curl https://www.coingecko.com/en/coins/dogecoin/historical_data#panel -o output.csv 
 html2text output.csv > parsed_data.csv 
 sed -n '/(Historical Data)/,$p' parsed_data.csv > price_data.csv 
 sed -n '/Date/,$p' price_data.csv > price_data2.csv  
@@ -23,11 +23,11 @@ sed -i '$ d' price_data2.csv
 sed -i '1d' price_data2.csv
 sed -i '1iDate Market_Cap Volume Open Close' price_data2.csv
 
-mv price_data2.csv data_usdt.csv
+mv price_data2.csv data_dogecoin.csv
 rm output.csv 
 rm parsed_data.csv 
 rm price_data.csv 
 
 
-echo "Scrap done"
- 
+echo "Created by Sacha Fiereder"
+echo "Scrap done !"
