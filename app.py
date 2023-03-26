6373b81b3e89c605c4b638f1815a2e4ff7068763
SachaFrdr <96545705+SachaFrdr@users.noreply.github.com>
2023-03-26 23:42:26 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,13 +4,13 @@ from dash import html
 import datetime
 
 
-btc_ticker = "BTC"
-xrp_ticker = "XRP"
+magic_ticker = "MAGIC"
+dogecoin_ticker = "DOGECOIN"
 
 # Convert to dataframe
 import pandas as pd
-data_btc = pd.read_csv("data_btc.csv", sep= " ", skipinitialspace=True)
-data_xrp = pd.read_csv("data_xrp.csv", sep= " ", skipinitialspace=True)
+data_magic = pd.read_csv("data_magic.csv", sep= " ", skipinitialspace=True)
+data_dogecoin = pd.read_csv("data_dogecoin.csv", sep= " ", skipinitialspace=True)
 
 
 # Open Dash application
@@ -24,10 +24,10 @@ app = dash.Dash(__name__, suppress_callback_exceptions=True)
 
 def update_ticker_graph(ticker, n):
 
-    if ticker == btc_ticker:
-        data = data_btc
+    if ticker == magic_ticker:
+        data = data_magic
     else:
-        data = data_xrp
+        data = data_dogecoin
     
     fig = {
         "data": [{"x": data["Date"], "y": data["Close"], "type": "line", "line": {"color": "red"}}],
@@ -47,17 +47,17 @@ def update_time(n):
 
 # Manage automatic updates
 app.layout = html.Div([
-    dcc.Interval(id="interval-component", interval=300 * 1000, n_intervals=0),
+    dcc.Interval(id="interval-component", interval= 5 * 60 * 1000, n_intervals=0),
     html.Div([
-        html.H1("Financial Dashboard : BTC et XRP (Lina FAIROUD)", style={'textAlign': 'center', 'color' : 'black', 'fontSize' : 30}),
+        html.H1("Financial Dashboard : MAGIC et DOGECOIN (Sacha FIEREDER)", style={'textAlign': 'center', 'color' : 'black', 'fontSize' : 30}),
         
         dcc.Dropdown(
             id="ticker-dropdown",
             options=[
-                {"label": "BTC", "value": btc_ticker},
-                {"label": "XRP", "value": xrp_ticker}
+                {"label": "MAGIC", "value": magic_ticker},
+                {"label": "DOGECOIN", "value": dogecoin_ticker}
             ],
-            value=btc_ticker,
+            value=magic_ticker,
             style={'color' : 'black'}
         ),
         
@@ -65,10 +65,10 @@ app.layout = html.Div([
             id="ticker-graph"
         ),
         
-        html.Div("Bitcoin (BTC) is a cryptocurrency, a virtual currency designed to act as money and a form of payment outside the control of any one person, group, or entity, thus removing the need for third-party involvement in financial transactions. It is rewarded to blockchain miners for the work done to verify transactions and can be purchased on several exchanges.", style={'textAlign': 'justify', 'color' : 'black', 'fontSize': 20}
+        html.Div("Magic", style={'textAlign': 'justify', 'color' : 'black', 'fontSize': 20}
         ),
 
-        html.Div("Ripple is a real-time, cryptocurrency gross-settlement system, currency exchange and remittance network created by Ripple Labs Inc, a US-based technology company. The company then created the XRP cryptocurrency, which it describes as a “digital asset built for global payments”.", style={'textAlign': 'justify', 'color' : 'black', 'fontSize': 20}
+        html.Div("Dogecoin", style={'textAlign': 'justify', 'color' : 'black', 'fontSize': 20}
         ),
         
         html.Div(style={'textAlign': 'right', 'color' : 'red'},
