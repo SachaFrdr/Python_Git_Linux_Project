f59b3f4458474c1d1c837133b46840185b85d312
SachaFrdr <96545705+SachaFrdr@users.noreply.github.com>
2023-03-27 00:44:25 +0200

    Update app.py

--- app.py
+++ app.py
@@ -75,6 +75,10 @@ app.layout = html.Div([
         html.Div("MAGIC is the native token of Treasure. Treasure is the decentralized video game console connecting games and communities together through imagination, MAGIC, and NFTs. Dogecoin is a cryptocurrency that was created on December 6th, 2013 based on the popular Doge Internet meme and features a Shiba Inu on its logo. The codebase of the project was a fork of Litecoin, in which most of the same features such hash hashing algorithm were inherited, with the only difference of branding and large inflationary supply. You may buy Dogecoin on centralized exchanges such as Binance, Crypto.com, Coinbase, Bitfinex, and more.", style={'textAlign': 'justify', 'color' : 'black', 'fontSize': 18}
         ),
         
+        html.Div("MAGIC Converter : 1 MAGIC = $1.46", style={'textAlign': 'justify', 'color' : 'black', 'fontSize': 18}),
+        
+        html.Div("DOGE Converter : 1 DOGE = $0.074644", style={'textAlign': 'justify', 'color' : 'black', 'fontSize': 18}),
+        
         html.Div(style={'textAlign': 'right', 'color' : 'blue'},
             id="update-time")
     ])
